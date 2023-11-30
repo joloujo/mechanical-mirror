dd46f93c064ee33991072a54517f7e05da207b18
Dominic Salmieri <dsalmieri@olin.edu>
2023-11-30 00:33:02 -0500

    I was overconfident...

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ This module runs on the Raspberry Pi on startup.
 
 from blink import blink
 from shutdown import shutdown_on_push
-import threading
+# import threading
 
 def main():
     """ Main entry point of the app """
-    threading.Thread(target=blink).start()
-    threading.Thread(target=shutdown_on_push).start()
+    blink()
+    # threading.Thread(target=blink).start()
+    # threading.Thread(target=shutdown_on_push).start()
 
 if __name__ == "__main__":
     """ This is executed when run from the command line """
