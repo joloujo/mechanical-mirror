3cad3fa1eb5d35e774589d3b5ed43fad4424fc43
Dominic Salmieri <joloujo28@gmail.com>
2023-12-03 16:12:46 -0500

    added raspi stuff to get_image

--- get_image.py
+++ get_image.py
@@ -1,13 +1,13 @@
 import os
 import numpy as np
+import cv2 as cv
 
 # import the correct libraries depending on the OS
 if os.name == 'nt':
-    import cv2 as cv
-        
-else : # assume it's the raspberry pi
-    # need to set up the camera for the raspberry pi
+    # all of the right libraries are already imported
     pass
+else : # assume it's the raspberry pi
+    from picamera2 import Picamera2, Preview 
 
 class imageGetter():
     def __init__(self):
@@ -22,8 +22,12 @@ class imageGetter():
         if self.os == 'nt':
             self.cam = cv.VideoCapture(0)
         else: # assume it's the raspberry pi
-            pass
-            # Set up the camera for the raspberry pi
+            self.cam = Picamera2()
+            
+            camera_config = self.cam.create_still_configuration(main={"size": (1920, 1080)}, lores={"size": (640, 480)}, display="lores")
+            self.cam.configure(camera_config)
+            
+            self.cam.start()
 
         """
         Initializes the image getter
@@ -60,7 +64,23 @@ class imageGetter():
             picture = np.asarray(self.cam.read()[1], dtype=np.uint8)
 
         else: # assume it's the raspberry pi
-            pass
-            # Get the image from the camera
+            
+            # Use the keyboard for user input and PyCam2 for the camera
 
+            # get background image
+            user_input = input("Press enter to take a picture of the background:")
+            if user_input != "":
+                return background, picture, True
+            background = np.asarray(self.cam.capture_image(), dtype=np.uint8)
+
+            # get picture image
+            user_input = input("Press enter to take a picture of the subject:")
+            if user_input != "":
+                return background, picture, True
+            picture = np.asarray(self.cam.capture_image(), dtype=np.uint8)
+
+        # TODO: Make the images the right color when you do the arduino stuff
+
+        cv.imshow('background', background)
+        cv.imshow('picture', picture)
         return background, picture, False
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from arduino_interface import arduinoInterface
-from image_converter import imageConverter
+# from image_converter import imageConverter
 from get_image import imageGetter
 import numpy as np
 import cv2 as cv
@@ -7,21 +7,22 @@ import cv2 as cv
 WIDTH = 3
 HEIGHT = 3
 
-# arduino_interface = arduinoInterface('sim', WIDTH, HEIGHT)
+'''
+arduino_interface = arduinoInterface('sim', WIDTH, HEIGHT)
 
-# states = np.asarray([
-#     [1, 0, 0],
-#     [1, 0, 0],
-#     [1, 0, 0]
-# ], dtype=np.bool_)
+states = np.asarray([
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+], dtype=np.bool_)
 
-# arduino_interface.display(states)
+arduino_interface.display(states)
 
-# arduino_interface.close()
+arduino_interface.close()
+'''
 
 image_getter = imageGetter()
 
 background, picture, _ = image_getter.get_images()
-cv.imshow('background', background)
-cv.imshow('picture', picture)
-cv.waitKey(5000)
+cv.waitKey(30000)
+
